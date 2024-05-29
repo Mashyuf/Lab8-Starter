00860526d800999abaaacbb5dc8055482fc24f81
Matthew Fang <64571966+Mashyuf@users.noreply.github.com>
2024-05-29 03:16:02 -0700

    Add service worker

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,36 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if ("serviceWorker" in navigator) {
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', (e) => {
+      // Steps B3-B6 will be *inside* the event listener's function created in B2
+      // B3. TODO - Register './sw.js' as a service worker (The MDN article
+      //            "Using Service Workers" will help you here)
+      const registerServiceWoker = async () => {
+        try {
+          const registration = await navigator.serviceWorker.register("/sw.js");
+          // B4. TODO - Once the service worker has been successfully registered, console
+          //            log that it was successful.
+          if (registration.installing) {
+            console.log("Service worker installing");
+          } else if (registration.waiting) {
+            console.log("Service worker installed");
+          } else if (registration.active) {
+            console.log("Service worker active");
+          }
+        } catch (err) {
+          // B5. TODO - In the event that the service worker registration fails, console
+          //            log that it has failed.
+          // STEPS B6 ONWARDS WILL BE IN /sw.js
+          console.error(`Service worker registration failed with ${err}`);
+        }
+      };
+      registerServiceWoker();
+    })
+  } else {
+    console.log('Service worker is not supported');
+  }
 }
 
 /**
@@ -69,13 +90,13 @@ async function getRecipes() {
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
   if (localStorage.length != 0) {
-    return localStorage.getItem('recipes');
+    return JSON.parse(localStorage.getItem('recipes'));
   }
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
   let recipes = [];
-  let numRecipes = 1;
+  let numRecipes = 0;
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
@@ -119,10 +140,6 @@ async function getRecipes() {
       }
     })
   })
-  
-  
-  
-  
 }
 
 /**
